7323f303d33bf7b2ad7ce16798dd58cd5c22bb67
Alex Ermolov <aaermolov@gmail.com>
2021-01-15 19:48:31 +0300

    pkgs: dev: tools: added goimports-reviser

--- default.nix
+++ default.nix
@@ -52,6 +52,9 @@ rec {
   gohack = pkgs.callPackage pkgs/development/tools/gohack/default.nix {
     inherit buildGoPackage fetchgit lib stdenv;
   };
+  goimports-reviser = pkgs.callPackage pkgs/development/tools/goimports-reviser/default.nix {
+    inherit buildGoPackage fetchgit stdenv;
+  };
   i3lock-color = pkgs.callPackage pkgs/applications/window-managers/i3/lock-color.nix { lib = mylib; };
   redis-tui = pkgs.callPackage pkgs/development/tools/redis-tui/default.nix { lib = mylib; };
   xkeysnail = pkgs.callPackage pkgs/tools/X11/xkeysnail/default.nix {
