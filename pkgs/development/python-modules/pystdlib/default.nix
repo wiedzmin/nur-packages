5917cf95c348910f953e80d9abcc3b375fd30683
Alex Ermolov <aaermolov@gmail.com>
2023-04-21 15:44:35 +0300

    development: python-modules: updated `pystdlib` version

--- pkgs/development/python-modules/pystdlib/default.nix
+++ pkgs/development/python-modules/pystdlib/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonPackage rec {
   pname = "pystdlib";
-  version = "unstable";
+  version = "0.9";
 
   src = fetchFromGitHub {
     owner = "wiedzmin";
     repo = "pystdlib";
-    rev = "82d2cfc64acc71ec7a69106e13f9ccf1c51348c6";
-    sha256 = "1l1i072mcsbjgyabya18w8w6vsghk6d023vhqy6d3s6srcfwfay8";
+    rev = "70e64709ce6bdda05eebfc5d9cd32b96e666e9ea";
+    sha256 = "1mqnp5bf04jjfg6vw4ydg9dw80m3fb8j4794kpqgff63zfv02gx4";
   };
 
   propagatedBuildInputs = with python3Packages; [ dmenu-python-ng libtmux notify2 papis-python-rofi pyfzf pygit2 xlib ];
