74bb7859e28a436acb62cd2310ca0b8706e0cde8
Alex Ermolov <aaermolov@gmail.com>
2024-07-22 00:54:27 +0300

    applications: misc: updated `xremap` version

--- pkgs/applications/misc/xremap/default.nix
+++ pkgs/applications/misc/xremap/default.nix
@@ -1,18 +1,19 @@
 { rustPlatform
 , fetchFromGitHub
+, libevdev
 ,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "xremap";
-  version = "v0.8.11";
+  version = "v0.10.0";
 
   src = fetchFromGitHub {
     owner = "k0kubun";
     repo = "xremap";
     rev = version;
-    sha256 = "sha256-ruJkOj4lDGCxhyrLhUcaDfIWv7QAUr49fqlvJySpD4c=";
+    sha256 = "sha256-TZvi5EOZ5Ekg8aGXCAzCcphJ7U5YsPtTWKTUQKZXEsg=";
   };
 
   buildFeatures = [ "x11" ]; # FIXME: elaborate some features setting logic
-  cargoSha256 = "sha256-KotTQriYiKsD/vpCbNvYDa9YQRlQxxLjQ+hg5+jaewU=";
+  cargoSha256 = "sha256-2a66jPpb/fdI4EBMUgYW3NTlQGKlsOAUYvZendHK9+o=";
 }
