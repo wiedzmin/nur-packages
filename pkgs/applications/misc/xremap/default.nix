c14b39a31d2cbf519303a4960a584c973eee123d
Alex Ermolov <aaermolov@gmail.com>
2025-01-05 03:17:59 +0300

    applications: misc: xremap: updated

--- pkgs/applications/misc/xremap/default.nix
+++ pkgs/applications/misc/xremap/default.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "xremap";
-  version = "v0.10.1";
+  version = "v0.10.3";
 
   src = fetchFromGitHub {
     owner = "k0kubun";
     repo = "xremap";
     rev = version;
-    sha256 = "sha256-TZvi5EOZ5Ekg8aGXCAzCcphJ7U5YsPtTWKTUQKZXEsg=";
+    sha256 = "sha256-6w3sLuGay+WPtQ1MtddvECtwh0U7EQUCDDQxkzYRe5o=";
   };
 
   buildFeatures = [ "x11" ]; # FIXME: elaborate some features setting logic
-  cargoHash = "sha256-G9hQMAjCSoSPuLboH3OxxvsJlDqZ+7t/beQXcDCp1FQ=";
+  cargoHash = "sha256-CitrCG56uB/y3g2y2KxC3czsNq7B9FjFrLjQZItw2wk=";
 
   meta.mainProgram = "xremap";
 }
